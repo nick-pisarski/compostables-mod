caa1c148e79fc01b8e9baecac738e9d282a703be
Pisarski, Nick <nick.pisarski@gmail.com>
2025-07-04 17:32:54 -0500

    Refactor wood chips and compost items; update recipes and descriptions. Add thumbnail image and improve info.json details.

--- compostables/data.lua
+++ compostables/data.lua
@@ -1,33 +1,38 @@
 -- Wood Chips
-local wood_chips_icon = "__compostables-mod__/graphics/wood-chips.png"
+local wood_chips_icon = "__compostables__/graphics/wood-chips.png"
 local wood_chips_item = {
     type = "item",
     name = "wood-chips",
+    localised_name = "Wood Chips",
+    item_group = "intermediate-products",
     icon = wood_chips_icon,
     icon_size = 64,
-    subgroup = "wood",
     order = "a",
+    stack_size = 100,
 }
 local wood_chips_recipe = {
     type = "recipe",
     name = "wood-chips",
+    localised_name = "Wood Chips",
     ingredients = {
         { type = "item", name = "wood", amount = 1 },
     },
-    result = "wood-chips",
-    result_count = 10,
+    results = {
+        { type = "item", name = "wood-chips", amount = 10 },
+    },
     energy_required = 1,
     category = "crafting",
 }
 
 
-local compost_icon = "__compostables-mod__/graphics/compost.png"
+local compost_icon = "__compostables__/graphics/compost.png"
 local compost_item = {
     type = "item",
     name = "compost",
+    localised_name = "Compost",
+    item_group = "intermediate-products",
     icon = compost_icon,
     icon_size = 64,
-    subgroup = "compost",
     order = "a",
     stack_size = 100,
     spoil_result = "landfill",
@@ -37,28 +42,32 @@ local compost_item = {
 -- Compost Recipes
 local compost_spoilage_recipe = {
     type = "recipe",
-    name = "compost",
+    name = "compost-mixing-spoilage",
+    localised_name = "Compost Mixing (Spoilage)",
     ingredients = {
-        { type = "item",  name = "spoilage", amount = 50 },
+        { type = "item",  name = "spoilage", amount = 10 },
         { type = "fluid", name = "water",    amount = 100 },
     },
-    result = "compost",
-    result_count = 50,
+    results = {
+        { type = "item", name = "compost", amount = 10 },
+    },
     energy_required = 1,
-    category = "crafting",
+    category = "crafting-with-fluid",
 }
 
 local compost_wood_chips_recipe = {
     type = "recipe",
-    name = "compost-wood-chips",
+    name = "compost-mixing-wood-chips",
+    localised_name = "Compost Mixing (Wood Chips)",
     ingredients = {
-        { type = "item",  name = "wood-chips", amount = 100 },
+        { type = "item",  name = "wood-chips", amount = 10 },
         { type = "fluid", name = "water",      amount = 100 },
     },
-    result = "compost",
-    result_count = 50,
+    results = {
+        { type = "item", name = "compost", amount = 10 },
+    },
     energy_required = 1,
-    category = "crafting",
+    category = "crafting-with-fluid",
 }
 
 data:extend({
--- scripts/zip_copy_mod.sh
+++ scripts/zip_copy_mod.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# Information
-author="RR"
-homepage_url=""
-factorio_version="2.0"
+# # Information
+# author="RR"
+# homepage_url=""
+# factorio_version="2.0"
 
-# Mod Information
-mod_name="compostables"
-mod_version="0.1.0"
-mod_title="Compostables"
-mod_description="Compostables"
-mod_dependencies="base >= 2.0"
+# # Mod Information
+# mod_name="compostables"
+# mod_version="0.1.0"
+# mod_title="Compostables"
+# mod_description="Compostables"
+# mod_dependencies="base >= 2.0"
 
 # Factorio Mod Info
 factorio_mods_folder="$HOME/AppData/Roaming/Factorio/mods"
 
-# Make info.json
-info_json="  {
-    \"name\": \"$mod_name\",
-    \"version\": \"$mod_version\",
-    \"factorio_version\": \"$factorio_version\",
-    \"title\": \"$mod_title\",
-    \"author\": \"$author\",
-    \"contact\": \"Private\",
-    \"homepage\": \"$homepage_url\",
-    \"description\": \"$mod_description\",
-    \"dependencies\": [\"$mod_dependencies\"],
-    \"spoiling_required\": true
-  }"
+# # Make info.json
+# info_json="  {
+#     \"name\": \"$mod_name\",
+#     \"version\": \"$mod_version\",
+#     \"factorio_version\": \"$factorio_version\",
+#     \"title\": \"$mod_title\",
+#     \"author\": \"$author\",
+#     \"contact\": \"Private\",
+#     \"homepage\": \"$homepage_url\",
+#     \"description\": \"$mod_description\",
+#     \"dependencies\": [\"$mod_dependencies\"],
+#     \"spoiling_required\": true
+#   }"
 
-echo -e "${info_json}" > "${mod_name}/info.json"
+# echo -e "${info_json}" > "${mod_name}/info.json"
 
-echo "Created 'info.json':"
+# echo "Created 'info.json':"
 
-echo -e "${info_json}"
+# echo -e "${info_json}"
 
-echo -e "\n################\n"
+# echo -e "\n################\n"
 
 #Zip
 # Check if dist folder exists, create if it doesn't
